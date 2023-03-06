fcc09eadefccea4559f19cc1e11b80c9f164e0a3
ifm-csr <support.robotics@ifm.com>
2023-03-06 08:24:08 +0000

    update docs

--- latest/cpp_api/classifm3d_1_1ODSOccupancyGridV1.js
+++ latest/cpp_api/classifm3d_1_1ODSOccupancyGridV1.js
@@ -6,6 +6,6 @@ var classifm3d_1_1ODSOccupancyGridV1 =
     [ "image", "classifm3d_1_1ODSOccupancyGridV1.html#a1f1b5be7c236e82b87fd8863d41af6d1", null ],
     [ "ods_occupancy_grid_v1_size", "classifm3d_1_1ODSOccupancyGridV1.html#a9c65cab732ac9d572e467c207e6777e6", null ],
     [ "timestamp_ns", "classifm3d_1_1ODSOccupancyGridV1.html#aa938e929499758ce52ab918a2784167c", null ],
-    [ "transfor_cell_center_to_user", "classifm3d_1_1ODSOccupancyGridV1.html#a485cf4ca7382470c4ff743637d65f4f6", null ],
+    [ "transform_cell_center_to_user", "classifm3d_1_1ODSOccupancyGridV1.html#ac05515e8832c59ed83ed1313565e66df", null ],
     [ "width", "classifm3d_1_1ODSOccupancyGridV1.html#adb4586cf789d8a1540a7aa4bc31eefd4", null ]
 ];
