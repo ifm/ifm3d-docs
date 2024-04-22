8711b21ee76de255f8bd56ee24cfbd8c8e99a122
ifm-csr <support.robotics@ifm.com>
2024-04-22 12:10:54 +0000

    update docs

---
+++ v1.5.3/cpp_api/modules.js
@@ -0,0 +1,9 @@
+var modules =
+[
+    [ "Common Module", "group__Common.html", "group__Common" ],
+    [ "Device Module", "group__Device.html", "group__Device" ],
+    [ "FrameGrabber Module", "group__FrameGrabber.html", "group__FrameGrabber" ],
+    [ "SWUpdater Module", "group__SWUpdater.html", "group__SWUpdater" ],
+    [ "PCICClient Module", "group__PCICClient.html", "group__PCICClient" ],
+    [ "Deserialize Module", "group__Deserialize.html", "group__Deserialize" ]
+];
