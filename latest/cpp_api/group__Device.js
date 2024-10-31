181e550a5e7bcf3427a5f918c5ca957883817f55
ifm-csr <support.robotics@ifm.com>
2024-10-31 02:18:01 +0000

    update docs

--- latest/cpp_api/group__Device.js
+++ latest/cpp_api/group__Device.js
@@ -79,7 +79,7 @@ var group__Device =
       [ "Error", "classifm3d_1_1Error.html#afab46311f4bb0a720bedc7fb87c3ff61", null ],
       [ "code", "classifm3d_1_1Error.html#a55941c65ba566878c000075bed9533b6", null ],
       [ "message", "classifm3d_1_1Error.html#aa1c693d09de6ccf38b576b58f11e5c54", null ],
-      [ "what", "classifm3d_1_1Error.html#a73277f5526471dcb49d0dfad0ce7fb69", null ]
+      [ "what", "classifm3d_1_1Error.html#ad353a2d6644bc18969109bcb73ab9f75", null ]
     ] ],
     [ "O3D", "classifm3d_1_1O3D.html", [
       [ "Ptr", "classifm3d_1_1O3D.html#ac5b4dd28c247500613ab0f74eb534fef", null ],
